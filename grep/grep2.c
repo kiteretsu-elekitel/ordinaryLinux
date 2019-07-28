f268d6bd62adc109b0ce4961b81502e2460d76d2
kiteretsu-elekitel <kiteretsu.elekitel@gmail.com>
2019-07-28 18:05:05 +0900

    implement option -i (ignore case) and -v (invert match)

--- grep/grep2.c
+++ grep/grep2.c
@@ -8,19 +8,26 @@
 
 #define BUFSIZE 1024;
 
-static void do_grep(regex_t *pat, FILE *f);
+static void do_grep(regex_t *pat, FILE *f, bool ignoreCase, bool invert);
+static void do_grep_ignore(regex_t *pat, FILE *f);
+static void toLowerCase(char *targetBuf);
 
 int main(int argc, char *argv[]) {
 
 	int opt;
+	int optNum = 0;
 	bool ignoreCase = false;
+	bool invert = false;
 
 	while ((opt = getopt(argc, argv, "iv")) != -1 ) {
 		switch(opt) {
 			case 'i':
 				ignoreCase = true;
+				optNum += 1;
 				break;
 			case 'v':
+				invert = true;
+				optNum += 1;
 				break;
 			case '?':
 				fputs("invalid argument given", stderr);
@@ -31,13 +38,17 @@ int main(int argc, char *argv[]) {
 	regex_t pat;
 	int err;
 	int i;
+	int patind = optind;
 
-	if (argc < 2) {
+	if ((argc - optNum) < 2) {
 		fputs("no pattern\n", stderr);
 		exit(1);
 	}
 
-	err = regcomp(&pat, argv[1], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+	if (ignoreCase) {
+		toLowerCase(argv[patind]);
+	}
+	err = regcomp(&pat, argv[patind], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
 	if (err != 0) {
 		char buf[BUFSIZ];
 
@@ -45,10 +56,10 @@ int main(int argc, char *argv[]) {
 		puts(buf);
 		exit(1);
 	}
-	if (argc == 2) {
-		do_grep(&pat, stdin);
+	if ((argc - optNum) == 2) {
+		do_grep(&pat, stdin, ignoreCase, invert);
 	} else {
-		for(i = 2; i < argc; i++) {
+		for(i = optind + 1; i < argc; i++) {
 			FILE *f;
 
 			f = fopen(argv[i], "r");
@@ -56,7 +67,7 @@ int main(int argc, char *argv[]) {
 				perror(argv[i]);
 				exit(1);
 			}
-			do_grep(&pat, f);
+			do_grep(&pat, f, ignoreCase, invert);
 			fclose(f);
 		}
 	}
@@ -65,21 +76,55 @@ int main(int argc, char *argv[]) {
 
 }
 
-static void do_grep(regex_t *pat, FILE *src) {
+static void do_grep(regex_t *pat, FILE *src, bool ignoreCase, bool invert) {
 	char buf[4096];
+	char orgBuf[4096];
 
 	while (fgets(buf, sizeof buf, src)) {
-		if (regexec(pat, buf, 0, NULL, 0) == 0) {
-			fputs(buf, stdout);
+		if (ignoreCase) {
+			memccpy(orgBuf, buf, '\0', sizeof(buf));
+			toLowerCase(buf);
 		}
+
+		int result = regexec(pat, buf, 0, NULL, 0);
+		if (!invert && result == 0) {
+			if (ignoreCase) {
+				fputs(orgBuf, stdout);
+			} else {
+				fputs(buf, stdout);
+			}
+		} else if (invert && result != 0) {
+			if (ignoreCase) {
+				fputs(orgBuf, stdout);
+			} else {
+				fputs(buf, stdout);
+			}
+		}
+
 	}
 }
 
-static char *toLowerCase(char *targetBuf) {
-	for(int i; i <= strlen(targetBuf); i++) {
-		if(targetBuf[i] >= 65 && targetBuf[i] <= 90)
+static void toLowerCase(char *targetBuf) {
+	for(int i = 0; i <= strlen(targetBuf); i++) {
+		if(targetBuf[i] >= 65 && targetBuf[i] <= 90) {
       		targetBuf[i] = targetBuf[i] + 32;
-	}
+		}
 
-	return targetBuf;
+	}
+}
+
+static void do_grep_ignore(regex_t *pat, FILE *src) {
+	char buf[4096];
+	char orgBuf[4096];
+	char* lowerCaseBuf;
+
+
+	while (fgets(buf, sizeof buf, src)) {
+		memccpy(orgBuf, buf, '\0', sizeof(buf));
+	    toLowerCase(buf);
+		//printf("%s", buf);
+		if (regexec(pat, buf, 0, NULL, 0) == 0) {
+			fputs(orgBuf, stdout);
+		}
+	}
 }
